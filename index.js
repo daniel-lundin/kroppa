0cf9e145dadc3c415e14126db04e791cf90bbfcb
Daniel Lundin <d-dog@d-dog.se>
2016-09-09 11:19:05 +0200

    Handle multiple crops in one call

--- index.js
+++ index.js
@@ -2,32 +2,40 @@
 
 const jpg = require('jpeg-turbo');
 
-function crop(buffer, x, y, cropWidth, cropHeight) {
+function extractCrop(buffer, x, y, width, height) {
+  const pixelSize = 3;
+  const rowWidth = buffer.width;
+  const rawData = buffer.data;
+
+  const pixelRows = Array(height);
+  for (let row = y; row < y + height; row++) {
+    const startPosition = (row * rowWidth * pixelSize) + (x * pixelSize);
+    const endPosition = startPosition + (width * pixelSize);
+    pixelRows[row - y] = rawData.slice(startPosition, endPosition);
+  }
+
+  const cropData = Buffer.concat(pixelRows, width * pixelSize * height);
+  return cropData;
+}
+
+function crop(buffer, cropData, quality = 100) {
+  const cropList = Array.isArray(cropData) ? cropData : [cropData];
+
   const decrompressedJpg = jpg.decompressSync(buffer, {
     format: jpg.FORMAT_RGB
   });
 
-  const pixelSize = 3;
-  const rowWidth = decrompressedJpg.width;
-  const rawData = decrompressedJpg.data;
-
-  const pixelRows = Array(cropHeight);
-  for (let row = y; row < y + cropHeight; row++) {
-    const startPosition = (row * rowWidth * pixelSize) + (x * pixelSize);
-    const endPosition = startPosition + (cropWidth * pixelSize);
-    pixelRows[row - y] = rawData.slice(startPosition, endPosition);
-  }
-
-  const cropData = Buffer.concat(pixelRows, cropWidth * pixelSize * cropHeight);
-
-  const output = jpg.compressSync(cropData, {
-    format: jpg.FORMAT_RGB,
-    width: cropWidth,
-    height: cropHeight,
-    quality: 100
+  const results = cropList.map((data) => {
+    const { x, y, width, height } = data;
+    const extractedCrop = extractCrop(decrompressedJpg, x, y, width, height);
+    return jpg.compressSync(extractedCrop, {
+      format: jpg.FORMAT_RGB,
+      width,
+      height,
+      quality
+    });
   });
-
-  return output;
+  return results.length === 1 ? results[0] : results;
 }
 
 module.exports = {
